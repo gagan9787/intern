6e4cf9f722899b5c748e9346c0eaf0c185ec030a
gagan9787 <gagan.courselelo@gmail.com>
2025-10-03 12:28:57 +0530

    Some Change

--- Project3/frontend/src/components/Footer.jsx
+++ Project3/frontend/src/components/Footer.jsx
@@ -7,7 +7,8 @@ export default function NavBar() {
     <nav className="bg-blue-600/80 backdrop-blur-md sticky top-0 z-50 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-center items-center">
         <h1 className="text-2xl font-extrabold tracking-tight text-white">
-         🙏 Thanks For Visit Us! 🙏
+         {/* 🙏 Thanks For Visit Us! 🙏 */}
+         © 2025 Community Services. All rights reserved.
         </h1>
       </div>
     </nav>
